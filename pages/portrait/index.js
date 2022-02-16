0619fddeb6f22ab241b33d0735f6986a10722975
Claire Peng <pwrclaire@gmail.com>
2022-02-15 23:14:18 -0800

    fxi

--- pages/portrait/index.js
+++ pages/portrait/index.js
@@ -10,7 +10,7 @@ export default function People() {
         {/* chris */}
         <div className="relative h-[90vh] md:h-[40vh]">
           <a a href="/portrait/chris">
-          <div class="opacity-0 hover:opacity-100 bg-black bg-opacity-30  duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">Chris & Raph</div>
+          <div class="opacity-0 hover:opacity-100 bg-black bg-opacity-30  duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl md:text-3xl text-white font-semibold">Chris & Raph</div>
             <Image
               className="shadow-md brightness-100 hover:brightness-50 object-center z-5"
               src="https://res.cloudinary.com/wrp/image/upload/v1642146257/winnie/chris_raph/DSC_3436_m2ezxi.jpg"
@@ -24,7 +24,7 @@ export default function People() {
         {/* jhan */}
         <div className="relative h-[90vh] md:h-[40vh]">
           <a a href="/portrait/jhan">
-          <div class="opacity-0 hover:opacity-100 bg-black bg-opacity-30  duration-300 absolute inset-0 z-10 flex justify-center items-center text-center text-6xl text-white font-semibold">Khushboo & J&nbsp;Han</div>
+          <div class="opacity-0 hover:opacity-100 bg-black bg-opacity-30  duration-300 absolute inset-0 z-10 flex justify-center items-center text-center text-6xl md:text-3xl text-white font-semibold">Khushboo & J&nbsp;Han</div>
             <Image
               className="shadow-md brightness-100 hover:brightness-50 object-right z-5"
               src="https://res.cloudinary.com/wrp/image/upload/v1642146256/winnie/kush_jhan/JHan_Khush4_epzld2.jpg"
