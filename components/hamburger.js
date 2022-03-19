c2b63e916c8c2ee6ab2d35c1c452bdd2d0a25c61
Claire Peng <pwrclaire@gmail.com>
2022-03-19 13:45:39 -0700

    resp

--- components/hamburger.js
+++ components/hamburger.js
@@ -2,14 +2,34 @@ import React, { useState } from "react";
 
 export default function Hamburger() {
   const [open, setOpen] = useState(false);
-  console.log('THis one hamburger.js', open);
+  console.log("THis one hamburger.js", open);
   return (
-    <nav
-      className={`absolute flex items-center justify-end p-6 w-full z-50 top-0 overflow-hidden h-72`}
-    >
-      <span>
+    <>
+      <div
+        className={`${
+          open ? "right-0" : "-right-60"
+        } top-20 absolute text-2xl text-right transition-all duration-500 pt-1 pl-4 pr-8 pb-2 bg-white`}
+      >
+        <div className="flex flex-col gap-2">
+          <a href="/automotive" className="hover:text-shadow-grey">
+            Automotive
+          </a>
+          <a href="/portrait" className="hover:text-shadow-grey">
+            Portrait
+          </a>
+          <a href="/wedding" className="hover:text-shadow-grey">
+            Wedding
+          </a>
+          <a href="/contact" className="hover:text-shadow-grey">
+            Contact
+          </a>
+        </div>
+      </div>
+      <nav
+        className={`absolute flex items-center justify-end p-6 w-full z-50 top-0 overflow-hidden h-24`}
+      >
         <button
-          className="text-[#4AB1A1] absolute top-12 right-6 bg-white w-[35px] h-[28px]"
+          className="text-[#4AB1A1] absolute top-12 right-10 bg-white w-[35px] h-[28px]"
           onClick={() => setOpen(!open)}
         >
           {/* <span className="sr-only">Open main menu</span> */}
@@ -20,12 +40,12 @@ export default function Hamburger() {
                 open ? "rotate-45 translate-y-2.5" : "-translate-y-0"
               }`}
             ></span>
-           <span
+            <span
               aria-hidden="true"
               className={`block absolute h-[3px] w-8 bg-current transform transition duration-500 ease-in-out translate-y-2.5 ${
                 open && "opacity-0"
               }`}
-            ></span> 
+            ></span>
             <span
               aria-hidden="true"
               className={`block absolute h-[3px] w-8 bg-current transform  transition duration-500 ease-in-out" ${
@@ -34,15 +54,7 @@ export default function Hamburger() {
             ></span>
           </div>
         </button>
-      </span>
-      <div className={`${open ? 'right-0' : '-right-60'} top-20 absolute text-3xl text-right transition-all duration-500 pt-1 pl-4 pr-8 pb-2 bg-white`}>
-        <div className="flex flex-col gap-2">
-          <a href='/automotive' className="hover:text-shadow-grey">Automotive</a>
-          <a href='/portrait' className="hover:text-shadow-grey">Portrait</a>
-          <a href='/wedding' className="hover:text-shadow-grey">Wedding</a>
-          <a href='/contact' className="hover:text-shadow-grey">Contact</a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
